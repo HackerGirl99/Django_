dd6ffb503f193596204037b8f8eb1083dbb76ef4
HackerGirl99 <asnaniyas98@gmail.com>
2024-03-13 15:07:04 +0530

    Python try

---
+++ qsn.py
@@ -0,0 +1,33 @@
+
+
+i=0
+while i<10:
+    if(i==5):
+continue
+i=i+1
+
+print()
+
+j=0
+while j<10:
+j=j+1
+if j==5:
+	continue
+	print(j)
+	
+	for x in "university":
+	print(x)
+
+print()
+
+for x in range(5):
+print(x)
+
+print()
+
+for x in range(2,9):
+print(x)
+
+print()
+for x in range(1,10,2):
+print(x)
